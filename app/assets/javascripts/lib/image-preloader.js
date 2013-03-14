e5f805eee32407a9d8ee70c8e9df29c3e9c1890d
Johan Kim <hiphapis@gmail.com>
2013-03-15 04:53:26 +0900

    stupid ie

--- app/assets/javascripts/lib/image-preloader.js
+++ app/assets/javascripts/lib/image-preloader.js
@@ -1,6 +1,8 @@
 // author: hiphapis@gmail.com
+var imageResult = {};
 var imagePreloader = function(images, callback, cache) {
 	var loadImage = function(url) {
+		imageResult[url]["status"] = "start";
 		// console.log("start: " + url);
 		var img = new Image();
 	  img.onload = function() { processLoadResponse(url) }
@@ -10,6 +12,8 @@ var imagePreloader = function(images, callback, cache) {
 	
 	var loaded = 0;	
 	var processLoadResponse = function(url) {
+		imageResult[url]["status"] = "done";
+
 		// console.log("ok: " + url);
 		callback(loaded += 1, images.length);
 	}
@@ -19,17 +23,20 @@ var imagePreloader = function(images, callback, cache) {
 	var processUnloadResponse = function(url) {
 		unloadedImages[url] = unloadedImages[url] + 1 || 1;
 		if (unloadedImages[url] >= retryCount) {
+			imageResult[url]["status"] = "failed";
 			// console.log("failed-fail: " + url);
 			processLoadResponse(url);
 			return
 		}
 		else {
+			imageResult[url]["status"] = "retry";
 			// console.log("failed-retry(" + unloadedImages[url] + "): " + url);
 			loadImage(url);
 		}
 	}
 
 	$.each(images, function(idx, url) {
+		imageResult[url] = {};
 		loadImage(url);
 	});
 	
