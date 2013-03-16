a798eda9249743a41fe76df1634c068710f9cb44
Johan Kim <hiphapis@gmail.com>
2013-03-16 16:48:52 +0900

    init github & mobile - loading

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Adidas::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'main#index'
+	get 'main' => 'main#index'
 	get 'iframe' => "main#iframe"
 
   # See how all your routes lay out with "rake routes"
